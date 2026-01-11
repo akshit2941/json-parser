7952e61f18547f90d0bd915c4af7f4e6fa4375c3
Akshit Saini <sainiakshit2941@gmail.com>
2026-01-11 17:41:38 +0530

    json from local path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,22 @@ import parser.lexer.Lexer;
 import parser.model.JsonValue;
 import parser.parser.JsonParser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Main  {
     public static void main(String[] args) {
-        String json = "{ \"name\": \"TestApp\", \"version\": 1, \"active\": true }";
+//        String json = "{ \"name\": \"TestApp\", \"version\": 1, \"active\": true }";
+
+        Path path = Path.of("data/test.json");
+        String json = null;
+        try {
+            json = Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         Lexer lexer = new Lexer(json);
         List<Token> tokens = lexer.tokenize();
